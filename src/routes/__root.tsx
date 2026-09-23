962642c4ed965e12d884f3e686a353865fac4851
chenye101 <cthoesli@gmail.com>
2026-09-23 12:57:01 -0600

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -90,7 +90,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-{ rel: "stylesheet", href: "https://api.fontshare.com/v2/css?f[]=satoshi@300,400,500&display=swap" },
+{ rel: "stylesheet", href: "https://api.fontshare.com/v2/css?f[]=satoshi@300,400,500,700&display=swap" },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
     ],
   }),
